2f78f66dd35d1950edeb1d14a46b5f25d9a3cabb
lonelyotter <57280827+lonelyotter@users.noreply.github.com>
2022-05-08 13:57:27 +0800

    frontend: fix bookcard warning

--- frontend/src/components/BookCard.js
+++ frontend/src/components/BookCard.js
@@ -1,34 +1,27 @@
 import React from "react";
-import {Card} from "antd";
-import {Link} from "react-router-dom";
+import { Card } from "antd";
+import { Link } from "react-router-dom";
 
 export default function BookCard(props) {
-    return (
-        <Link
-            to={{
-                pathname: "/book/" + props.book.id,
-            }}
-        >
-            <Card
-                hoverable
-                bordered={false}
-                cover={
-                    <img
-                        src={props.book.image}
-                        alt={props.book.name}
-                        loading
-                    />
-                }
-            >
-                <p style={{fontWeight: "bold", fontSize: "large"}}>
-                    {props.book.name}
-                </p>
-                <p style={{color: "red", fontWeight: "bold"}}>
-                    ¥ {props.book.price}
-                </p>
-                <p style={{color: "gray"}}>库存{props.book.inventory}件</p>
-            </Card>
-        </Link>
-    );
-
+  return (
+    <Link
+      to={{
+        pathname: "/book/" + props.book.id,
+      }}
+    >
+      <Card
+        hoverable
+        bordered={false}
+        cover={
+          <img src={props.book.image} alt={props.book.name} loading="true" />
+        }
+      >
+        <p style={{ fontWeight: "bold", fontSize: "large" }}>
+          {props.book.name}
+        </p>
+        <p style={{ color: "red", fontWeight: "bold" }}>¥ {props.book.price}</p>
+        <p style={{ color: "gray" }}>库存{props.book.inventory}件</p>
+      </Card>
+    </Link>
+  );
 }
